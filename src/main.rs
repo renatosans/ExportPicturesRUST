6306c4b477adb2249f72736a9fe59e52d759230a
renatosans <renato@datacopy.com.br>
2023-10-13 05:22:46 -0300

    Ajustes

--- src/main.rs
+++ src/main.rs
@@ -267,6 +267,7 @@ fn export_picture(product: Produto, output_dir: String) {
         return
     });
     file.flush().unwrap_or_else(|e| {
+
         println!("Error: {}", e);
         return
     });
